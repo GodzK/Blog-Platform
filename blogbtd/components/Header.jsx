254c2f591583f719218c8de68dfc61db89e74147
Phakaphol <64467457+GodzK@users.noreply.github.com>
2024-03-27 14:57:51 +0700

    s

--- blogbtd/components/Header.jsx
+++ blogbtd/components/Header.jsx
@@ -8,9 +8,9 @@ function Header() {
       <div className='header-name'>
         <h1>Hi Im <span style={{ color: "blueviolet" }}>Pk Phakaphol Dherachaisuphakij</span> </h1>
         <p>front-end Developer Section from Bortodev Devinit#2</p>
-        <a href='https://www.facebook.com/phakaphol.dherachaisuphakij/'><img src="../public/fb.png" alt=""  /></a>
-        <a href='https://www.instagram.com/pk._tcsk/'><img src="../public/ig.png" alt=""  /></a>
-        <a href='https://www.linkedin.com/in/phakaphol-dherachaisuprakij-1092ab256/'><img src="../public/li.png" alt=""  /></a> <br/>  
+        <a href='https://www.facebook.com/phakaphol.dherachaisuphakij/'><img src="../public/images/fb.png" alt=""  /></a>
+        <a href='https://www.instagram.com/pk._tcsk/'><img src="../public/images/ig.png" alt=""  /></a>
+        <a href='https://www.linkedin.com/in/phakaphol-dherachaisuprakij-1092ab256/'><img src="../public/images/li.png" alt=""  /></a> <br/>  
         </div>
       <div className='img'>
         <img src={pk} alt="" />
