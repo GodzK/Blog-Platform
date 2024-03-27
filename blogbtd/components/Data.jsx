c1746618264044d32c9b8967d47c4e8430da9b2f
Phakaphol <64467457+GodzK@users.noreply.github.com>
2024-03-27 15:46:46 +0700

    insensitive

--- blogbtd/components/Data.jsx
+++ blogbtd/components/Data.jsx
@@ -1,28 +1,41 @@
-import React,{useState} from 'react';
-import Home from "./Home";
+import React, { useState } from 'react';
+import Home from './Home';
 import Form from 'react-bootstrap/Form';
-import { BrowserRouter, NavLink, } from 'react-router-dom';
+import { BrowserRouter, NavLink } from 'react-router-dom';
 import { db } from './db';
 import { InputGroup } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 function Data() {
-  const [search , setSearch] = useState('');
+  const [search, setSearch] = useState('');
+
   return (
     <>
       <BrowserRouter>
-      <Form>
-        <InputGroup className="w-30 p-3">
-          <Form.Control  placeholder="Search Blog"  onChange={(e) => setSearch(e.target.value)} />
-         </InputGroup>
-      </Form>
-        <div className="grid-container">       
-          {db.filter((item) =>{
-            return search.toLowerCase() ==="" ? item : item.text.toLowerCase().includes(search)
-          }).map((item) => (
-            <NavLink key={item.id} to={`https://www.borntodev.com/?p=${item.id}`}>
-              <Home img={item.img} text={item.text} tech1={item.tech1} tech2={item.tech2} tech3={item.tech3} />
-            </NavLink>
-          ))}
+        <Form>
+          <InputGroup className="w-30 p-3">
+            <Form.Control
+              placeholder="Search Blog"
+              onChange={(e) => setSearch(e.target.value.toLowerCase())} // Convert lowercase
+            />
+          </InputGroup>
+        </Form>
+        <div className="grid-container">
+          {db
+            .filter((item) => {
+              return search === '' ? item : item.text.toLowerCase().includes(search); // Compare ทั้งคู่ ใน lowercase
+            })
+            .map((item) => (
+              <NavLink key={item.id} to={`https://www.borntodev.com/?p=${item.id}`}>
+                <Home
+                  img={item.img}
+                  text={item.text}
+                  tech1={item.tech1}
+                  tech2={item.tech2}
+                  tech3={item.tech3}
+                />
+              </NavLink>
+            ))}
         </div>
       </BrowserRouter>
     </>
