708eca0e6abbe5e1f21c3e8535778d7a392e3308
Phakaphol Dherachaisuphakij <64467457+GodzK@users.noreply.github.com>
2024-04-02 11:49:20 +0700

    add / images on app mobile only

--- blogbtd/components/Header.jsx
+++ blogbtd/components/Header.jsx
@@ -10,9 +10,9 @@ function Header() {
         <h1>Hi Im <span style={{ color: "blueviolet" }}>Pk Phakaphol Dherachaisuphakij</span> </h1>
         <p>front-end Developer Section from Bortodev Devinit#2</p>
         <div className='social'>
-           <a href='https://www.facebook.com/phakaphol.dherachaisuphakij/'><img src="../public/images/fb.png" alt=""  /></a>
-        <a href='https://www.instagram.com/pk._tcsk/'><img src="../public/images/ig.png" alt=""  /></a>
-        <a href='https://www.linkedin.com/in/phakaphol-dherachaisuprakij-1092ab256/'><img src="../public/images/li.png" alt=""  /></a> <br/>  
+           <a href='https://www.facebook.com/phakaphol.dherachaisuphakij/'><img src="/images/fb.png" alt=""  /></a>
+        <a href='https://www.instagram.com/pk._tcsk/'><img src="/images/ig.png" alt=""  /></a>
+        <a href='https://www.linkedin.com/in/phakaphol-dherachaisuprakij-1092ab256/'><img src="/images/li.png" alt=""  /></a> <br/>  
         </div>
        
         </div>
