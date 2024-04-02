9ba1b42465c93c4fe50a300f1032d8c2c1cab174
Phakaphol Dherachaisuphakij <64467457+GodzK@users.noreply.github.com>
2024-04-02 15:32:11 +0700

    .

--- blogbtd/components/statistic.jsx
+++ blogbtd/components/statistic.jsx
@@ -1,89 +1,93 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-
-import './statistic.css'
+import Stack from '@mui/material/Stack';
+import { Gauge } from '@mui/x-charts/Gauge';
+import Grid from '@mui/material/Grid';
+import './statistic.css';
 import db from './db.json';
 
-function statistic(props) {
+function Statistic(props) {
   const totalViewers = db.reduce((accumulator, currentValue) => accumulator + currentValue.viewer, 0);
   const totalBlogs = db.length;
+  
   return (
     <div className="container text-center p-5">
- <Box xs={{ maxWidth: 50 }}>
-      <Card variant="outlined"><React.Fragment>
-    <CardContent>
-      <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-        <p>All Blog</p>
-      </Typography>
-      <Typography variant="h5" component="div">
-        <p>Total Viewer is {totalViewers} </p>
-      </Typography>
-      <Typography sx={{ mb: 1.5 }} color="text.secondary">
-       Active
-      </Typography>
-     
-    </CardContent>
-    
-  </React.Fragment></Card>
-    </Box>
-    <Box xs={{ maxWidth: 50 }}>
-      <Card variant="outlined"><React.Fragment>
-    <CardContent>
-      <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-        <p>All Blog</p>
-      </Typography>
-      <Typography variant="h5" component="div">
-        <p>Total Blog is {totalBlogs}</p>
-      </Typography>
-      <Typography sx={{ mb: 1.5 }} color="text.secondary">
-       Active
-      </Typography>
-     
-    </CardContent>
-    
-  </React.Fragment></Card>
-    </Box>
-    <Box xs={{ maxWidth: 50 }}>
-      <Card variant="outlined"><React.Fragment>
-    <CardContent>
-      <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-        <p>All Blog</p>
-      </Typography>
-      <Typography variant="h5" component="div">
-        <p>asdad</p>
-      </Typography>
-      <Typography sx={{ mb: 1.5 }} color="text.secondary">
-       Active
-      </Typography>
-     
-    </CardContent>
-    
-  </React.Fragment></Card>
-    </Box>
-    <Box xs={{ maxWidth: 50 }}>
-      <Card variant="outlined"><React.Fragment>
-    <CardContent>
-      <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-        <p>All Blog</p>
-      </Typography>
-      <Typography variant="h5" component="div">
-        <p>asdad</p>
-      </Typography>
-      <Typography sx={{ mb: 1.5 }} color="text.secondary">
-       Active
-      </Typography>
-     
-    </CardContent>
-    
-  </React.Fragment></Card>
-    </Box>
-</div>
-  )
+      <Grid container spacing={3} className='gridd' >
+        
+        <Grid item xl={12} xs={12} sm={6} >
+          <Card variant="outlined">
+            <CardContent className='bg-success p-3'>
+              <Typography variant="h6" component="div" color="text.secondary" gutterBottom>
+                All Blog
+              </Typography>
+              <Typography variant="h4" component="div">
+                Total Viewer is {totalViewers}
+              </Typography>
+              <Typography color="text.secondary">
+                Active
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        
+        <Grid item xl={12} xs={12} sm={6}>
+          <Card variant="outlined">
+            <CardContent className='bg-danger p-3'>
+              <Typography variant="h6" component="div" color="text.secondary" gutterBottom>
+                All Blog
+              </Typography>
+              <Typography variant="h4" component="div">
+                Total Blog is <br /> {totalBlogs} 
+              </Typography>
+              <Typography color="text.secondary">
+                Active
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        
+        <Grid item xl={12} xs={12} sm={6}>
+          <Card variant="outlined">
+            <CardContent className='bg-white p-3'>
+              <Typography variant="h6" component="div" color="text.secondary" gutterBottom>
+                People love this
+              </Typography>
+              <Typography variant="h4" component="div" className="d-flex justify-content-center align-items-center">
+                <Stack>
+                  <Gauge width={120} height={120} value={60} />
+                </Stack>
+              </Typography>
+              <Typography color="text.secondary">
+                Active
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        
+        <Grid item xl={12} xs={12} sm={6}>
+          <Card variant="outlined">
+            <CardContent className='bg-white p-3'>
+              <Typography variant="h6" component="div" color="text.secondary" gutterBottom>
+                People love this
+              </Typography>
+              <Typography variant="h4" component="div" className="d-flex justify-content-center align-items-center">
+                <Stack>
+                  <Gauge width={120} height={120} value={60} />
+                </Stack>
+              </Typography>
+              <Typography color="text.secondary">
+                Active
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+      
+      </Grid>
+    </div>
+  );
 }
 
-export default statistic
+export default Statistic;
