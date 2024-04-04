8276932145c464a4204c20a7be4fc2f6588634b5
GodzK <64467457+GodzK@users.noreply.github.com>
2024-04-04 13:22:08 +0700

    del social

--- blogbtd/components/Header.jsx
+++ blogbtd/components/Header.jsx
@@ -9,12 +9,8 @@ function Header() {
       <div className='header-name'>
       <h1 style={{ color: "#fff" }}>Hi Im <span style={{ color: "blueviolet" }}>Pk Phakaphol Dherachaisuphakij</span> </h1>
         <p>front-end Developer Section from Bortodev Devinit#2</p>
-        <div className='social'>
-           <a href='https://www.facebook.com/phakaphol.dherachaisuphakij/'><img src="/images/fb.png" alt=""  /></a>
-        <a href='https://www.instagram.com/pk._tcsk/'><img src="/images/ig.png" alt=""  /></a>
-        <a href='https://www.linkedin.com/in/phakaphol-dherachaisuprakij-1092ab256/'><img src="/images/li.png" alt=""  /></a> <br/>  
-        </div>
-       
+        <p><br />
+I dreamed that one day I would become a skilled frontend developer with the experience to teach others to understand code effortlessly and wanted to advance technology further.</p>
         </div>
       <div className='img'>
         <img src={pk} alt="" />
